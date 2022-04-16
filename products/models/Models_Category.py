51538a3ccd703abe09ae8d3108d000a1540fae87
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-16 14:11:49 +0000

    Fix a typo.

--- products/models/Models_Category.py
+++ products/models/Models_Category.py
@@ -3,11 +3,11 @@ from products.models import *
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendy_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     def get_friendly_name(self):
-        return self.friendy_name
+        return self.friendly_name
     
