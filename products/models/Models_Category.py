6f9150419483a391ea82470163e8f53e0987875b
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-16 14:24:07 +0000

    Add a Meta class with verbose name to Category class.

--- products/models/Models_Category.py
+++ products/models/Models_Category.py
@@ -2,6 +2,10 @@ from products.models import *
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
