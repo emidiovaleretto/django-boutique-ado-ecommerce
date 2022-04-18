dc286abf1485d4901f30513c6518e0a619deca02
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-18 16:05:23 +0000

    Add cart context and delivery logic.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_cart(request):
+    """ This function renders the cart content. """
+    return render(request, 'cart/cart.html')
