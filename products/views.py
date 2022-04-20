06c5fe87e5b49dde050b1444ce736a8197641d84
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-20 16:43:34 +0000

    Import messages.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.contrib import messages
 from products.models.Models_Product import Product
 from products.models.Models_Category import Category
 
