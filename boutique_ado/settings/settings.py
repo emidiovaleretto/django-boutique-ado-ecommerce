33e0b774163e0e8af8f841c3f9d5b33b2fdf2e80
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-20 16:25:22 +0000

    Add MESSAGE_STORAGE variable in settings.py

--- boutique_ado/settings/settings.py
+++ boutique_ado/settings/settings.py
@@ -97,6 +97,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 # Password validation
