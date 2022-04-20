085c60e30fc1676e8a931b9633ab7be8d0e94d52
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-20 15:57:22 +0000

    Implement a function to delete an item from cart and add a URL path to it.

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
     path('adjust/<item_id>', views.adjust_quantity_to_cart, name='adjust_quantity_to_cart'),
+    path('remove/<item_id>', views.remove_item_from_cart, name='remove_item_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -39,32 +39,6 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
-def remove_item_from_cart(request, item_id):
-    """ This function removes the item from the shopping cart. """
-
-    quantity = int(request.POST.get('quantity'))
-    size = None
-
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-
-    cart = request.session.get('cart', {})
-
-    if size:
-        if quantity > 0:
-            cart[item_id]['items_by_size'][size] = quantity
-        else:
-            del cart[item_id]['items_by_size'][size]
-
-    else:
-        if quantity > 0:
-            cart[item_id] = quantity
-        else:
-            cart.pop[item_id]
-
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
-
 def adjust_quantity_to_cart(request, item_id):
     """ This function adjust the quantity of an specific product
         to the specific amount. """
@@ -82,12 +56,37 @@ def adjust_quantity_to_cart(request, item_id):
             cart[item_id]['items_by_size'][size] = quantity
         else:
             del cart[item_id]['items_by_size'][size]
-
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
     else:
         if quantity > 0:
             cart[item_id] = quantity
         else:
-            cart.pop[item_id]
+            cart.pop(item_id)
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_item_from_cart(request, item_id):
+    """ This function removes the item from the shopping cart. """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
