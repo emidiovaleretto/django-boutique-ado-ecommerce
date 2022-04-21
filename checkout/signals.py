5d57e9727218aeec2bb6e572834a009021afbe82
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-21 23:16:57 +0000

    Fix the function's name and remove an unused parameter.

--- checkout/signals.py
+++ checkout/signals.py
@@ -12,7 +12,7 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     This function updates the order total on lineitem update/create
     """
