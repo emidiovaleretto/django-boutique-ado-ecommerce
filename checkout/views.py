cf42d4c40cf4aba0d1c15da6e8dcfb9933bb5568
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-21 19:31:36 +0000

    Create Order form and check if it's valid.

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
 import os
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
+from .models import OrderLineItem
+from products.models import Product
 
 from cart.contexts import cart_contents
 
@@ -17,21 +19,70 @@ def checkout(request):
     stripe_public_key = os.environ.get("STRIPE_PUBLIC_KEY")
     stripe_secret_key = os.environ.get("STRIPE_SECRET_KEY")
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your bag at the moment.")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address_1': request.POST['street_address_1'],
+            'street_address_2': request.POST['street_address_2'],
+            'town_or_city': request.POST['town_or_city'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+            'county': request.POST['county'],
+        }
 
-    current_bag = cart_contents(request)
-    total = current_bag['grand_total']
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
+                try:
+                    product = get_object_or_404(Product, pk=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=item_data,
+                                product_size=size
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, "One of the products in your bag wasn't found in our database. "
+                                            "Please call us for assistance!")
+                    order.delete()
+                    return redirect(reverse('view_cart'))
 
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'Something went wrong while trying to submit your form. '
+                                    'Please double check your information.')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(
+                request, "There's nothing in your bag at the moment.")
+            return redirect(reverse('products'))
 
-    payment_intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
+        current_bag = cart_contents(request)
+        total = current_bag['grand_total']
+
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+
+        payment_intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
 
     order_form = OrderForm()
 
