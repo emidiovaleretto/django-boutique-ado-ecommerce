2028c173b8a88605cd028b5b232383b2fe9322ab
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-25 15:42:54 +0000

    Add methods to handle event from Stripe.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,6 +15,26 @@ class StripeWHookHandler:
         unexpected webhook event.
         """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        This method handles the payment_intent.succeeded
+        webhook event from Stripe.
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        This method handles the payment_intent.payment_failed
+        webhook event from Stripe.
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200
         )
