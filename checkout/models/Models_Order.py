6e303cabc5b20cc02c970bbfcdac276ddb748dc3
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-26 14:52:05 +0000

    Create to additional fields in the order models.

--- checkout/models/Models_Order.py
+++ checkout/models/Models_Order.py
@@ -26,6 +26,9 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_payment_id = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
