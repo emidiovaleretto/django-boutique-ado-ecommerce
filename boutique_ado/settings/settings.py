ec05cc318368fefdaba3ce45eb35b4bc0ec9cdd5
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-29 18:44:54 +0000

    Expose port.

--- boutique_ado/settings/settings.py
+++ boutique_ado/settings/settings.py
@@ -174,9 +174,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
